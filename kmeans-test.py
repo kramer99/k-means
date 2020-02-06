0a5169783521b3f20b2ac9bf52afdeb9f4b5917f
kramer <kramer@air.local>
2020-02-06 14:41:40 -0800

    tests with guassian clumped samples

--- kmeans-test.py
+++ kmeans-test.py
@@ -2,18 +2,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 import kmeans
 
-def twoD():
+def uniformTwoD():
     m = 100  # sample size
     n = 2   # number of features
     K = 3   # number of clusters
     X = np.random.uniform(0, 100, (m,n))
     clusterings = kmeans.run(X, K)
 
-def threeD():
+def uniformThreeD():
     m = 200  # sample size
     n = 3   # number of features
     K = 4   # number of clusters
     X = np.random.uniform(0, 100, (m,n))
     clusterings = kmeans.run(X, K)
 
-twoD()
+def clusteredTwoD():
+    m = 100  # sample size
+    n = 2   # number of features
+    K = 3   # number of clusters
+    X = np.zeros((0,n))
+    centersOfMass = np.random.uniform(0, 100, (K,n))
+    for i in centersOfMass:
+        stdDev = 12
+        samples = np.random.normal(i, stdDev, (int(m / K), n))
+        X = np.append(X, samples, axis = 0)
+    clusterings = kmeans.run(X, K)
+
+def clusteredThreeD():
+    m = 200  # sample size
+    n = 3   # number of features
+    K = 4   # number of clusters
+    X = np.zeros((0,n))
+    centersOfMass = np.random.uniform(0, 100, (K,n))
+    for i in centersOfMass:
+        stdDev = 12
+        samples = np.random.normal(i, stdDev, (int(m / K), n))
+        X = np.append(X, samples, axis = 0)
+    clusterings = kmeans.run(X, K)
+
+clusteredTwoD()
